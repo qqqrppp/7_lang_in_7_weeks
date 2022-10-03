2d17e398ea165a854f51a1ef561b58e39e28205c
axell <dimje2@gmail.com>
2022-10-03 04:42:42 +0300

    update ruby example for collect

--- ruby_first_week.rb
+++ ruby_first_week.rb
@@ -1,3 +1,8 @@
-5.times { puts 'hello world'  }
+puts 'hello ruby'
 
-[1..5].map {|x| puts "Hello World" }
+5.times { puts 'hello ruby' }
+
+(1..5).map { |x| puts "hello ruby #{x}" }
+
+list = (1..5).map { |x| "hello world #{x}" }
+list.map {|text| puts text.sub 'world', 'ruby'}
