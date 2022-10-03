a538ba145eec77db5ef0ece72a08c0e1f4239eb4
axell <dimje2@gmail.com>
2022-10-03 04:53:42 +0300

    add comments

--- ruby_first_week.rb
+++ ruby_first_week.rb
@@ -1,8 +1,13 @@
-puts 'hello ruby'
+# collection, map fn, puts and replace
+
+puts 'hello world of ruby'
 
 5.times { puts 'hello ruby' }
 
-(1..5).map { |x| puts "hello ruby #{x}" }
+(1..5).map { |x| puts "hello #{x}" }
 
 list = (1..5).map { |x| "hello world #{x}" }
+
+puts list
+
 list.map {|text| puts text.sub 'world', 'ruby'}
